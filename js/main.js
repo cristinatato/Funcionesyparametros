e18bd806a275ebce35a436df875f5e5bb9cd1434
cristinatato <c.tatodelatorre@gmail.com>
2019-07-30 10:06:26 +0200

    add fase4

--- js/main.js
+++ js/main.js
@@ -6,17 +6,28 @@ function ten(str,maxNumber) {
     // console.log(`${i}patata`)
     }   
 }
-
-ten('Patata', 10);
-ten('Aguacate', 7);
-ten('Pizza', 50);
-// esto llama a la función, si no se llama no funciona.ç
-
-
 // function ten(paco) {
-//     for (let i=0; i<paco.max; i++) {
-//         console.log(paco.text);
-//     }
-// }
+    //     for (let i=0; i<paco.max; i++) {
+        //         console.log(paco.text);
+        //     }
+        // }
+        
+        // ten({text:'patata', max:10});
 
-// ten({text:'patata', max:10});
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+// const randomNumber = getRandomNumber(50); esto no hace falta, porque entonces me generaría el mismo número para los tres.
+
+ten('Patata', getRandomNumber(50));
+ten('Aguacate', getRandomNumber(50));
+ten('Pizza', getRandomNumber(50));
+
+
+
+// ten('Patata', 10);
+// ten('Aguacate', 7);
+// ten('Pizza', 50);
+        // esto llama a la función, si no se llama no funciona.ç
