5e928589ebf00e37da856a2015ad7e6334899faf
cristinatato <c.tatodelatorre@gmail.com>
2019-07-30 09:44:09 +0200

    add fase1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function ten() {
+    for (let i=0; i<10; i++)
+    console.log(i,'patata');
+    // console.log(`${i}patata`)
+}
+
+ten(); 
+// esto llama a la función, si no se llama no funciona.
