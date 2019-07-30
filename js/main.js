8ae339f0d0244fa2253ab3eb8b472a72295d4c09
cristinatato <c.tatodelatorre@gmail.com>
2019-07-30 09:59:26 +0200

    add fase3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 'use strict';
 
-function ten(str) {
-    for (let i=0; i<10; i++)
+function ten(str,maxNumber) {
+    for (let i=0; i<maxNumber; i++) {
     console.log(str);
     // console.log(`${i}patata`)
+    }   
 }
 
-ten('Patata');
-ten('Aguacate');
-ten('Pizza');
-// esto llama a la función, si no se llama no funciona.
+ten('Patata', 10);
+ten('Aguacate', 7);
+ten('Pizza', 50);
+// esto llama a la función, si no se llama no funciona.ç
+
+
+// function ten(paco) {
+//     for (let i=0; i<paco.max; i++) {
+//         console.log(paco.text);
+//     }
+// }
+
+// ten({text:'patata', max:10});
